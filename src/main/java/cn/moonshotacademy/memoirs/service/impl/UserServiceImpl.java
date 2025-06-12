04921b00975759b7ffe6aec32441f810cda6f459
vio3500 <vio3500@github>
2025-06-12 13:14:27 +0800

    Add Thumbnailator dependency for image processing in UserServiceImpl

--- src/main/java/cn/moonshotacademy/memoirs/service/impl/UserServiceImpl.java
+++ src/main/java/cn/moonshotacademy/memoirs/service/impl/UserServiceImpl.java
@@ -4,7 +4,6 @@ import cn.moonshotacademy.memoirs.config.FileProperties;
 import cn.moonshotacademy.memoirs.dto.AvatarDto;
 import cn.moonshotacademy.memoirs.dto.EmailDto;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import cn.moonshotacademy.memoirs.dto.UserDto;
