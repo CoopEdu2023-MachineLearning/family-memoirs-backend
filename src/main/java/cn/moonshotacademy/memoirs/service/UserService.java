46c914f12ca03c4b273ea0eefea535e4210f4b84
vio3500 <vio3500@github>
2025-06-12 14:24:37 +0800

    Refactor EmailController to remove unused import statement for RequestBody

--- src/main/java/cn/moonshotacademy/memoirs/service/UserService.java
+++ src/main/java/cn/moonshotacademy/memoirs/service/UserService.java
@@ -19,8 +19,6 @@ public interface UserService {
 
     public void changeUserName(int id, UserDto username);
 
-    public void changeUserEmail(int id, String newEmail);
-
     public void changeUserPassword(int id, String oldPassword, String newPassword);
 
     public void changeUserAvatar(int id, AvatarDto requestData) throws IOException;
